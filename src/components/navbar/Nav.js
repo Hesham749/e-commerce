46d5e1953088f824dc49602ba3d8d49a5fcd9a5e
Hesham749 <heesham749@gmail.com>
2024-03-30 17:24:42 +0200

    update-nav-toggle

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -69,7 +69,10 @@ export default function Nav() {
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-sticky"
-              aria-expanded="false">
+              aria-expanded="false"
+
+              data-dropdown-toggle="navbar-sticky"
+              >
               <span className="sr-only">Open main menu</span>
               <div className="">
                 <svg
